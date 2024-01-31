76dacba10caa06323072fbd1800c530d8bbb0397
Marko Elez <markoelez7@gmail.com>
2024-01-31 10:32:53 -0500

    fix path

--- minislam2/main.cc
+++ minislam2/main.cc
@@ -36,7 +36,7 @@ Point2f fit_point(const int rows, const int cols, Mat mat_3b1) {
 
 int main() {
     // TODO: make config dynamic
-    std::string base_path = "../eval/test2";
+    std::string base_path = "../eval/test";
 
     const auto fx = 718.856;
     const auto fy = 718.856;
